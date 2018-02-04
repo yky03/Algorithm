9567d3878c4a4d7edeee1e51a192478bf06be3dd
yky <ykycome00#naver.com>
2018-02-04 10:00:05 +0900

    [7주차] 문자열사용하기 , 아스키코드

---
+++ Algorithem/src/week7/Quiz_11654.java
@@ -0,0 +1,13 @@
+package week7;
+
+import java.util.Scanner;
+
+public class Quiz_11654 {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		String n = scan.next();
+		System.out.println((int)n.charAt(0));
+	
+	}
+}
